9327f38a7806ac83b46f437bd15cc67181fa94b4
kevin <me@kzh.dev>
2024-12-09 08:23:59 -0500

    add plausible analytics

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -52,6 +52,9 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
+      <head>
+        <script defer data-domain="kzh.dev" src="https://plaus.mxph.dev/js/script.js"></script>
+      </head>
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
